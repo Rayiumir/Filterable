dad333b7b3543d42c5f95a23ccac32beb17d168b
Raymond Baghumian <swordart2050@gmail.com>
2024-04-27 19:37:20 +0330

    Comment JavaScript For Filterable

--- js/filterable.js
+++ js/filterable.js
@@ -8,20 +8,25 @@
  *
  */
 
-
+// Select all Filter Buttons and Cards
 const filterableButtons = document.querySelectorAll(".filterable button");
 const filterableCards = document.querySelectorAll(".cards .card");
 
+// Define the Cards Function
 const filterCards = e => {
     document.querySelector(".active").classList.remove("active");
     e.target.classList.add("active");
 
+    // Iterate Over Each Filterable Card
     filterableCards.forEach(card => {
+        // Adding "hide" Class to Hide the Card Inintially
         card.classList.add("hide");
+        //Check if the Card Matches the Selected Filter or "all" is selected
         if(card.dataset.name === e.target.dataset.name || e.target.dataset.name === "all"){
             card.classList.remove("hide");
         }
     });
 };
 
+//Adding Click Event Listener to Each Fillter Buton
 filterableButtons.forEach(button => button.addEventListener("click", filterCards));
