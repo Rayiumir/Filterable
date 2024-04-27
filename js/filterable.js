61509241ebd0099b510a426f78baa069f8d46a83
Raymond Baghumian <swordart2050@gmail.com>
2024-04-27 17:52:29 +0330

    Creating Files CSS, JS and Folder Fonts With Font Fira Code

---
+++ js/filterable.js
@@ -0,0 +1,9 @@
+/**!
+ * Filterable v1.0.0
+ * A simple, lightweight library for Filter the Items
+ *
+ * @copyright Copyright 2024 Rayiumir Limited.
+ * @author    Raymond Baghumian.
+ * @link      https://github.com/Rayiumir/Filterable
+ *
+ */
